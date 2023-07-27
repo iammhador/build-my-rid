d5a33085ea7cf9510a3800ac509ff822e2f81bbb
mehedy3107 <mhador111@gmail.com>
2023-07-28 02:40:15 +0600

    Fact: Basic setup

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ErrorPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h2 className="uppercase text-6xl text-gray-700 font-bold">
+        404! Page not found!!
+      </h2>
+    </div>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import RootLayout from "@/components/layout/root";
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      {getLayout(<Component {...pageProps} />)}
+    </SessionProvider>
+  );
 }
